941c919e8d1721b914523ae1932352a22964e69f
Arniel <aceba1@outlook.com>
2020-11-25 23:57:37 -0500

    Placeholder comment

--- src/main/java/com/aceba1/cd/capstone/processor/TransactionRepository.java
+++ src/main/java/com/aceba1/cd/capstone/processor/TransactionRepository.java
@@ -3,5 +3,5 @@ package com.aceba1.cd.capstone.processor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
+  //Page<Transaction> findBy____(____ ____, Pageable pageable);
 }
