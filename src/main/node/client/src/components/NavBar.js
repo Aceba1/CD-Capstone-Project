d19088e7b428bfe5f00edd695555d2808f79fcec
Arniel <aceba1@outlook.com>
2020-12-14 01:38:01 -0500

    Update NavBar.js

--- src/main/node/client/src/components/NavBar.js
+++ src/main/node/client/src/components/NavBar.js
@@ -4,11 +4,16 @@ import Spacer from './Spacer'
 
 export default function NavBar() {
   return (
-    <nav style={{display: "flex", flexDirection: "row"}}>
-      <Button text="Home" onClick={() => {window.location = "/"}} className="navbar-button button-login" />
-      <Spacer />
-      <Button text="Tables" onClick={() => {window.location = "/tables"}} className="navbar-button button-login" />
-      <Button text="Login" onClick={() => {window.location = "/user/login"}} className="navbar-button button-login" />
-    </nav>
+    <div className="header">
+      <label className="header-title" onClick={() => {window.location = "/"}}>
+        Prototype Site
+      </label>
+      <nav style={{display: "flex", flexDirection: "row"}}>
+        <Button text="Home" onClick={() => {window.location = "/"}} className="navbar-button button-login" />
+        <Spacer />
+        <Button text="Tables" onClick={() => {window.location = "/tables"}} className="navbar-button button-login" />
+        <Button text="Login" onClick={() => {window.location = "/user/login"}} className="navbar-button button-login" />
+      </nav>
+    </div>
   )
 }
