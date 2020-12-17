ebbd69b6515cfcd4ec71415b72317787b862114d
Arniel <aceba1@outlook.com>
2020-12-17 02:28:44 -0500

    Add cors to gateway

--- src/main/node/gateway/index.js
+++ src/main/node/gateway/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: require('path').resolve(
   process.cwd(), '../../resources/application.properties') });
 
 const express = require('express');
+const cors = require('cors');
 //const https = require('https'); //TODO: Research further
 
 const apiSwitch = require('./apiSwitch');
@@ -11,6 +12,7 @@ const port = 5910;
 
 app.use(express.json());
 
+app.use(cors());
 app.use(apiSwitch);
 
 // After all setup
