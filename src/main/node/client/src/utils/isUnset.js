b5fc7a988aceaa5a9419b1d463fa2253e85e06a9
Arniel <aceba1@outlook.com>
2020-12-17 02:28:55 -0500

    Check for empty string

--- src/main/node/client/src/utils/isUnset.js
+++ src/main/node/client/src/utils/isUnset.js
@@ -1,3 +1,3 @@
 export default function isUnset(value) {
-  return value === undefined || value === null;
+  return value === undefined || value === null || (value instanceof String && value.trim === 0);
 }
