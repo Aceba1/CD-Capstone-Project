23a7c8a75c0e1dca65e6fbbb2ef1af88f67922da
Arniel <aceba1@outlook.com>
2020-12-17 02:29:59 -0500

    Add gateway test

--- src/main/node/client/src/components/routes/UserManagementPage.js
+++ src/main/node/client/src/components/routes/UserManagementPage.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext'
+import Button from '../Button'
+import Input from '../Input'
 import BasicPage from './BasicPage'
 
-function UserManagementPage(props) {
+function UserManagementPage() {
+  const {jwt, setJwt, verifyJwt} = useContext(UserContext);
+
   return (
     <BasicPage className="page-user">
       <h1>Manage User</h1>
+
+      {/* Temporary! */}
+      <Input value={jwt} setValue={setJwt} ph="JWT" />
+      <Button text="Verify JWT" onClick={verifyJwt} />
+
     </BasicPage>
   )
 }
