ec4ed82db52131060ec0dcb76d140fb18ba45556
Arniel <aceba1@outlook.com>
2020-12-16 23:13:19 -0500

    Use Axios, relocate config json

--- src/main/node/client/src/utils/gateway.js
+++ src/main/node/client/src/utils/gateway.js
@@ -1,12 +1,20 @@
+import Axios from 'axios'
 import config from './gatewayConfig.json'
-
+/**
+ * 
+ * @param {string} action 
+ * @param {{} | string} body 
+ * @param {(status: number, body: {}) => void} onSuccess 
+ * @param {(reason: any) => void} onError 
+ */
 export default function gateway(action, body, onSuccess, onError) {
-  fetch(config.endpoint, {
-    body: JSON.stringify({
-      action: action,
-      body: body
-    }),
-    method: config.method
-  }).then(r => onSuccess(r.status, r.json))
+  Axios.request({
+    url: config.endpoint,
+    method: config.method,
+    data: {
+      action,
+      body
+    }
+  }).then(r => onSuccess(r.status, r.data))
   .catch(onError)
 }
