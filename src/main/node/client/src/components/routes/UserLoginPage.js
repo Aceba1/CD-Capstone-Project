2ac0b0a291882855059460726eb20823d7ded834
Arniel <aceba1@outlook.com>
2020-12-19 13:55:23 -0500

    Implpement checkingLogin to login page display

--- src/main/node/client/src/components/routes/UserLoginPage.js
+++ src/main/node/client/src/components/routes/UserLoginPage.js
@@ -6,14 +6,27 @@ import UserBasicPage from './UserBasicPage'
 
 function UserLoginPage(props) {
 
-  const { cred, setCred, pass, setPass, attemptLogin } = useLoginState();
+  const { cred, setCred, pass, setPass, attemptLogin, checkingLogin } = useLoginState();
 
   return (
     <UserBasicPage>
       <h1>Login User</h1>
-      <Input ph="Credential" value={cred} setValue={setCred} />
-      <Input ph="Password" value={pass} setValue={setPass} type="password" />
-      <Button text="Login" onClick={attemptLogin} />
+      { checkingLogin ? (
+        <>
+        <span style={{fontStyle: "italic"}} >Logging in...</span>
+        </>
+      ) : (
+        <>
+        <Input ph="Credential" value={cred} setValue={setCred} /><br />
+        <Input ph="Password" value={pass} setValue={setPass} type="password" /><br />
+        <Button text="Login" onClick={attemptLogin} /><br />
+        <br />
+        <div>
+          Need an account?
+          <Button text="Register" onClick={() => window.location = "/user/register" + window.location.search} />
+        </div>
+        </>
+      )}
     </UserBasicPage>
   )
 }
