69e08cd5eff77a2e312bdbb30c95772a7bf0ef92
Arniel <aceba1@outlook.com>
2020-12-20 00:55:27 -0500

    Alternate navbar content for login

--- src/main/node/client/src/components/NavBar.js
+++ src/main/node/client/src/components/NavBar.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
 import Button from './Button'
 import Spacer from './Spacer'
 
 export default function NavBar() {
+  const { loggedIn, name } = useContext(UserContext);
   return (
     <div className="header">
       <label className="header-title" onClick={() => {window.location = "/"}}>
@@ -12,7 +14,13 @@ export default function NavBar() {
         <Button text="Home" onClick={() => {window.location = "/"}} className="navbar-button button-login" />
         <Spacer />
         <Button text="Tables" onClick={() => {window.location = "/tables"}} className="navbar-button button-login" />
-        <Button text="Login" onClick={() => {window.location = "/user/login"}} className="navbar-button button-login" />
+        {
+          loggedIn ? (
+            <Button text={"User " + name} onClick={() => {window.location = "/user"}} className="navbar-button button-user" />
+          ) : (
+            <Button text="Login" onClick={() => {window.location = "/user/login"}} className="navbar-button button-login" />
+          )
+        }
       </nav>
     </div>
   )
